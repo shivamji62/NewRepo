8f078123f2752821dd16b4f2e3f4b0a02414c645
shivamji62 <shivam.dongre@moreyeahs.com>
2023-08-23 16:55:34 +0530

    Additionaly Commit

--- Generic/MovieManagementAPI/Program.cs
+++ Generic/MovieManagementAPI/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MovieManagementDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("myConn")));
